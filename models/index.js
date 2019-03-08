bb4216e83b9026be680d4e9ca2e19d96af200755
apal21 <apalshah21121996@gmail.com>
2019-03-08 09:40:31 +0530

    fixed mongoose deprecated index warning

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/auth', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/auth', { useNewUrlParser: true, useCreateIndex: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection Error'));
